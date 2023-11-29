71401e871b57c380c72340186b8a963a6207e337
donatech <donatien.nka@gmail.com>
2023-11-29 10:30:52 +0100

    Mise en place des fonctions bookAvailable qui permet d'indiqué si un livre est disponible ou non. deleteRow qui permet de supprimer le livre selectionné de la librairie.

--- scripts/script.js
+++ scripts/script.js
@@ -122,14 +122,14 @@ function renderTable() {
     //on efface la zone dans laquelle la table doit être placée
     table.innerHTML = ''
     for (let index = 0; index < cloudLibData.length; index++) {
-      let newRow = table.insertRow(index);
-      newRow.insertCell(0).innerText = cloudLibData[index].title;
-      newRow.insertCell(1).innerText = cloudLibData[index].author;
-      newRow.insertCell(2).innerText = cloudLibData[index].pages;
-      newRow.insertCell(3).innerHTML = `<span id='instock${index}'>${cloudLibData[index].instock}</span>`;
-      newRow.insertCell(4).innerHTML = `<button onclick='bookAvailable(${index})' class='tableButtons'>Check</button>`;    
-      newRow.insertCell(5).innerHTML = `<button onclick='showDetails(${index})' class='tableButtons' 
-      data-bs-toggle='modal' data-bs-target='#displaybookdetails'>details</button>`
+        let newRow = table.insertRow(index);
+        newRow.insertCell(0).innerText = cloudLibData[index].title;
+        newRow.insertCell(1).innerText = cloudLibData[index].author;
+        newRow.insertCell(2).innerText = cloudLibData[index].pages;
+        newRow.insertCell(3).innerHTML = `<span id='instock${index}'>${cloudLibData[index].instock}</span>`;
+        newRow.insertCell(4).innerHTML = `<button onclick='bookAvailable(${index})' class='tableButtons'>Check</button>`;    
+        newRow.insertCell(5).innerHTML = `<button onclick='showDetails(${index})' class='tableButtons' 
+        data-bs-toggle='modal' data-bs-target='#displaybookdetails'>details</button>`
     }
   
 }
@@ -140,27 +140,27 @@ function renderTable() {
  * à la fin de l'opération, elle vide le formulaire
  */
 function addBookToLibrary () { 
-  try {
-    let newTitle = validerSaisie(document.getElementById('title').value);
-    //est ce que un livre ayant ce titre est déjà enregistré dans la librairie ?
-    comparerTitre(newTitle);
-    let newAuthor = validerSaisie(document.getElementById('author').value);
-    let newGenre = document.getElementById('genre').value;
-    let newEditor = validerSaisie(document.getElementById('editor').value);
-    let newPages = validerPages(document.getElementById('pages').value);
-    let newPub_year = document.getElementById('publication-year').value;
-    let newResune = document.getElementById('resume').value;
-    let newPrice = document.getElementById('currency-field').value;    
-    let newCover = document.getElementById('cover-image').value;
-    let newStock = document.getElementById('flexCheckDefault').value;
-    let newRegdate = recordDate();    
-    newCover = getPath(newCover);
-    //console.log(newCover);
-    new newBook(newTitle, newAuthor, newGenre, newEditor, newPages, newPub_year,
-      newResune, newPrice, newCover, newStock, newRegdate);
-    //on vide les champs du formulaire
-    viderFormulaire();
-  }catch(erreur) {
+    try {
+        let newTitle = validerSaisie(document.getElementById('title').value);
+        //est ce que un livre ayant ce titre est déjà enregistré dans la librairie ?
+        comparerTitre(newTitle);
+        let newAuthor = validerSaisie(document.getElementById('author').value);
+        let newGenre = document.getElementById('genre').value;
+        let newEditor = validerSaisie(document.getElementById('editor').value);
+        let newPages = validerPages(document.getElementById('pages').value);
+        let newPub_year = document.getElementById('publication-year').value;
+        let newResune = document.getElementById('resume').value;
+        let newPrice = document.getElementById('currency-field').value;    
+        let newCover = document.getElementById('cover-image').value;
+        let newStock = document.getElementById('flexCheckDefault').value;
+        let newRegdate = recordDate();    
+        newCover = getPath(newCover);
+        //console.log(newCover);
+        new newBook(newTitle, newAuthor, newGenre, newEditor, newPages, newPub_year,
+          newResune, newPrice, newCover, newStock, newRegdate);
+        //on vide les champs du formulaire
+        viderFormulaire();
+    }catch(erreur) {
         viderFormulaire();
         window.alert(erreur.message);
         //console.log(erreur.message);         
@@ -172,14 +172,48 @@ function addBookToLibrary () {
  * Cette fonction permet de réinitialiser le formulaire
  */
 function viderFormulaire() {
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-  document.getElementById('pages').value = '';
-  document.getElementById('editor').value = '';
-  document.getElementById('genre').value = '';
-  document.getElementById('publication-year').value = '';
-  document.getElementById('resume').value = '';
-  document.getElementById('currency-field').value = '';
-  document.getElementById('cover-image').value = '';
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('pages').value = '';
+    document.getElementById('editor').value = '';
+    document.getElementById('genre').value = '';
+    document.getElementById('publication-year').value = '';
+    document.getElementById('resume').value = '';
+    document.getElementById('currency-field').value = '';
+    document.getElementById('cover-image').value = '';
+}
+
+
+/**
+ * Cette fonction permet d'indiquer si un livre est disponible
+ * (Yes) ou pas (No)
+ * @param {number} index 
+ */
+function bookAvailable(index) {
+    if (myLibrary[index].instock == 'No') {
+        myLibrary[index].instock = 'Yes'
+        document.getElementById(`instock${index}`).innerHTML = `<span id='instock${index}'>${myLibrary[index].instock}</span>`
+    } else {
+        myLibrary[index].instock = 'No'
+        document.getElementById(`instock${index}`).innerHTML = `<span id='instock${index}'>${myLibrary[index].instock}</span>`
+    }
+    writeUserData()
+}
+
+/**
+ * Cette fonction permet de supprimer l'objet donc l'index est transmit en 
+ * argument
+ * @param {number} index : index du livre à supprimer
+ */
+function deleteRow(index) {
+    if (index > 1) {
+        myLibrary.splice(index, index-1);
+    } else if (index == 1) {
+        myLibrary.splice(index, index);
+    } else if (index == 0) {
+        myLibrary.shift();
+    }
+    //ensuite on met à jour l'affichage de la table
+    writeUserData()
 }
 
