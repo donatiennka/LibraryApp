27410c499b5ae4a065bfe1bf52022e5482482f18
donatech <donatien.nka@gmail.com>
2023-11-29 10:15:22 +0100

    Mise en place de la fonction qui crée (affiche) en temps réel la table des livres. Cette fonction sera appelé chaque fois qu'il y aura une modification de la librairie.

--- scripts/script.js
+++ scripts/script.js
@@ -114,3 +114,23 @@ function getPath(cover) {
     return filename
 }
 
+/**
+ * Cette fonction crée la table des livres 
+ * elle est appelé chaque fois que le la librairie est modifiée
+ */
+function renderTable() {
+    //on efface la zone dans laquelle la table doit être placée
+    table.innerHTML = ''
+    for (let index = 0; index < cloudLibData.length; index++) {
+      let newRow = table.insertRow(index);
+      newRow.insertCell(0).innerText = cloudLibData[index].title;
+      newRow.insertCell(1).innerText = cloudLibData[index].author;
+      newRow.insertCell(2).innerText = cloudLibData[index].pages;
+      newRow.insertCell(3).innerHTML = `<span id='instock${index}'>${cloudLibData[index].instock}</span>`;
+      newRow.insertCell(4).innerHTML = `<button onclick='bookAvailable(${index})' class='tableButtons'>Check</button>`;    
+      newRow.insertCell(5).innerHTML = `<button onclick='showDetails(${index})' class='tableButtons' 
+      data-bs-toggle='modal' data-bs-target='#displaybookdetails'>details</button>`
+    }
+  
+}
+
